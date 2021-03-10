8c0a5f90921c94ea90ba0dbadc85871782fe2ee2
Jayant Dhawan <jayant.dhawan@codinova.com>
2021-03-10 20:06:42 +0530

    deploy

---
+++ js/chunk-2d2138f6.85608512.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2138f6"],{acca:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},r=[],u={name:"Projects"},a=u,l=t("2877"),o=Object(l["a"])(a,c,r,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d2138f6.85608512.js.map
